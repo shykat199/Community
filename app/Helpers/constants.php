217e0f9ce79acd71a2f2c10f7665eb65d175dfb2
shykat199 <shykatroybdku199@gmail.com>
2023-04-06 17:05:58 +0600

    helper file in heler folder

---
+++ app/Helpers/constants.php
@@ -0,0 +1,5 @@
+<?php
+
+const  ADMIN_ROLE = 1;
+const  USER_ROLE = 2;
+
---
+++ app/Models/Community/Group.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Community;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Group extends Model
+{
+    use HasFactory;
+}
