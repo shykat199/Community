c246b1d63cfd42ea9f1e629db6b6b7bf6f84c51e
ritusaha080 <ritusaha080@gmail.com>
2023-04-11 16:09:07 +0600

    user role add to register blade

--- resources/views/admin/auth/register.blade.php
+++ resources/views/admin/auth/register.blade.php
@@ -45,6 +45,16 @@
                 @enderror
             </div>
 
+            <article class="regular">
+                <h5>User Role</h5>
+                <input type="radio" name="user" id="rad1" value="provider">
+                <label for="rad1">Service Provider</label>
+                <input type="radio" name="user" id="rad2" value="vendor">
+                <label for="rad2">Vendor</label>
+                <input type="radio" name="user" id="rad3" value="user">
+                <label for="rad3">User</label>
+            </article>
+
             <div class="mb-3">
                 <div class="form-check">
                     <input type="checkbox" class="form-check-input" id="checkbox-signup">
